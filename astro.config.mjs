8855b6b25c2ba47c109def9b16b2e6c8d4b4da82
jdsherrington <mail@jdsherrington.com>
2024-02-05 00:37:30 +1100

    add RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://example.com",
+	site: "https://main--chipper-twilight-54a601.netlify.app/",
 	integrations: [],
 	server: { host: true, port: 3000 },
 });
