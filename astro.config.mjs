663b502e9d5f332c3981827e834f5da9aab2e3cc
jdsherrington <mail@jdsherrington.com>
2024-02-05 00:36:57 +1100

    add pages, styles, and scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://example.com",
 	integrations: [],
 	server: { host: true, port: 3000 },
 });
